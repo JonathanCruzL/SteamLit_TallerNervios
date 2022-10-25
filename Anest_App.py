45f2ac3bcb80b2a1696c6a7a8458abbaedac2c22
JonathanCruzL <100337650+JonathanCruzL@users.noreply.github.com>
2022-10-25 14:32:50 -0500

    Update Anest_App.py

--- Anest_App.py
+++ Anest_App.py
@@ -1,5 +1,6 @@
 
 import streamlit as st
+import matplotlib.pyplot as plt
 import numpy as np
 import cv2 as cv
 import pandas as pd
@@ -29,7 +30,7 @@ def camara():
         st.image(uploaded_file.getvalue())
         # Decodificar la imagen para ser  leida como una lista
 #         imagen = cv.imdecode(np.frombuffer(uploaded_file.getvalue(), np.uint8), cv.IMREAD_GRAYSCALE)
-        imagen = cv.imread(np.frombuffer(uploaded_file.getvalue(), np.uint8), cv.IMREAD_GRAYSCALE)
+        imagen = plt.imread(np.frombuffer(uploaded_file.getvalue(), np.uint8), cv.IMREAD_GRAYSCALE)
         # Convertir la lista en array
         img_array = np.array(imagen)
         # Creación del modelo
