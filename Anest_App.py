5d4fecbb68965247e771d64aa3662b6423b944d2
JonathanCruzL <100337650+JonathanCruzL@users.noreply.github.com>
2022-10-25 13:38:52 -0500

    Update Anest_App.py

--- Anest_App.py
+++ Anest_App.py
@@ -1,8 +1,8 @@
 
 import streamlit as st
 import numpy as np
-import cv2 as cv
-from cv import *
+import cv2 #as cv
+from cv2 import *
 import pandas as pd
 from unet import unet
 from proceso import (imagenProceso, removerAreas, aumentoTam, cuadrarRect,
